c9b68daf72589b41126055685c05c8239628d2d3
Meshkat <meshkat.hashemian@yahoo.com>
2023-11-02 04:24:27 +0330

    2nd question modified

--- Main2.java
+++ Main2.java
@@ -1,3 +1,27 @@
+//import java.util.*;
+//import java.util.function.Function;
+//import java.util.stream.Collectors;
+//
+//public class Main2 {
+//
+//    public static void main(String[] args) {
+//
+//        // Get the List
+//        List<String> stringList = Arrays.asList("maktab", "is", "maktab", "sharif", "maktab", "is", "good");
+//
+//        // Collect the list as map
+//        // by groupingBy() method
+//        Map<String, Long> result
+//                = stringList.stream().collect(
+//                Collectors.groupingBy(
+//                        Function.identity(),
+//                        Collectors.counting()));
+//
+//        // Print the result
+//        System.out.println(result);
+//    }
+//}
+//
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -6,19 +30,23 @@ public class Main2 {
 
     public static void main(String[] args) {
 
-        // Get the List
-        List<String> stringList = Arrays.asList("maktab", "is", "maktab", "sharif", "maktab", "is", "good");
+        List<String> stringList = Arrays.asList("Sajjad", "Meshkat", "Mahdi", "Mojtaba", "Mohammad", "Ali", "Taher", "Reza", "Mohsen");
 
-        // Collect the list as map
-        // by groupingBy() method
-        Map<String, Long> result
-                = stringList.stream().collect(
-                Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()));
+        // {"Amir", "Hatef", "Mahdi", "Mojtaba", "Mohammad", "Ali", "Taher", "Reza", "Mohsen"};
 
-        // Print the result
-        System.out.println(result);
+
+        Map<String, Integer> sorted = stringList.stream()
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.toMap(Function.identity(), String::length));
+
+        System.out.println(" question 1 :");
+        System.out.println(sorted);
+
+        Map<Integer, List<String>> groupingBy = stringList.stream().collect(Collectors.groupingBy(String::length));
+        System.out.println(" question 2 :");
+
+        System.out.println(groupingBy);
     }
 }
 
